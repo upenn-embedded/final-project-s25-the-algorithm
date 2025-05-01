a151fb3b75e4f3fedacd1735771aac768790a85e
Eric Li <ericli27@seas.upenn.edu>
2025-05-01 02:30:41 -0400

    cleaned up main.c

--- main.c
+++ main.c
@@ -1,25 +1,25 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <avr/io.h>
-//#include <avr/delay.h>
-//#include <avr/interrupt.h>
+/* main.c file for ScrewBot */
 
-#define STEPPER_MOTOR_CONTROL_AND_JOYSTICK
+/* choose one of three modes to define */
+#define STEPPERS_JOYSTICK_ACCELEROMETER
+// #define STEPPERS_JOYSTICK
 // #define END_EFFECTOR_INTERRUPT
-// #define END_EFFECTOR_POLLING
-// #define GYRO
 
+#include <avr/interrupt.h>
 #include <avr/io.h>
-#include <util/delay.h>
-#include <stdint.h>
 #include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "uart.h"
+#include <util/delay.h>
 
 #define F_CPU 16000000UL
 #define SCL_CLOCK 100000L
 #define RAD_TO_DEG 57.2957795f
 #define FILTER_WINDOW 50
 
+#ifdef STEPPERS_JOYSTICK_ACCELEROMETER
 static void TWI_init(void);
 static uint8_t TWI_start(void);
 static void TWI_stop(void);
@@ -158,13 +158,11 @@ int main(void) {
         while (1);
     }
     
-
     gyro_init();
     DDRB |= (1 << PB5);
     DDRB |= (1 << PB4);
     
-    
-    // Calibration
+    // Calibration of acceleromter
     _delay_ms(2000);
     float pitch_offset = 0.0f, roll_offset = 0.0f;
     
@@ -239,22 +237,11 @@ int main(void) {
          _delay_ms(100);
     }
 }
-
+#endif
 
 
 #ifdef STEPPER_MOTOR_CONTROL_AND_JOYSTICK
-/* THIS IS BEN'S CODE PLUS CODE FOR JOYSTICK CONTROL FOR THE 3 STEPPER MOTORS */
-#define F_CPU 16000000UL
-
-#include <avr/io.h>
-#include <util/delay.h>
-#include <stdio.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <avr/io.h>
-#include <avr/delay.h>
-
+/* code for stepper motor control using joystick, without accelerometer readings*/
 
 void Initialize() {
     DDRB |= (1 << PB0);
@@ -271,13 +258,11 @@ void set_dir_ccw(){
     PORTB |= (1 << PB1);
     PORTB |= (1 << PB3);
     PORTD |= (1 << PD7);
-    // PORTD &= ~(1 << PD1);
 }
 
 void set_dir_cw(){
     PORTB &= ~(1 << PB1);
     PORTB &= ~(1 << PB3);
-    // PORTD |= (1 << PD1);
     PORTD &= ~(1 << PD7);
 }
 
@@ -326,107 +311,30 @@ uint16_t adc_read() {
     return ADCW;
 }
 
-//int main(int argc, char** argv) {
-//   
-//    Initialize();
-//    
-//    uart_init();
-//    adc_init();
-//
-//    while (1) {
-//        uint16_t adcVal = adc_read();
-//        printf("ADCVAL = %d\r\n", adcVal);
-//
-//        if (adcVal > 300) {
-//            set_dir_cw();
-//            send_pulse();
-//        } else if (adcVal < 100) {
-//            set_dir_ccw();
-//            send_pulse();
-//        }
-//        PORTB &= ~(1 << PB0);
-//        PORTB &= ~(1 << PB2);
-//        PORTD &= ~(1 << PD0);
-//    }
-//}
-#endif
-
-/* THIS IS THE CODE FOR THE END EFFECTOR MOTOR */
-
-
-#ifdef END_EFFECTOR_POLLING
-#include <xc.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
-#include <stdio.h>
-
-void Initialize() {
-    DDRB |= (1 << PB0) | (1 << PB1);  // IN1 and IN2 as output
-    DDRB &= ~((1 << PB2) | (1 << PB3)); // PB2 and PB3 as input
-
-    PORTB |= (1 << PB2) | (1 << PB3); // Enable pull-ups on inputs (optional)
-
-    PCICR |= (1 << PCIE0);    // Enable pin change interrupt for PCINT[7:0]
-    PCMSK0 |= (1 << PCINT2);  // Enable interrupt on PB2 (PCINT2)
-    
-    // Setup for ADC (10bit = 0-1023)
-    // Clear power reduction bit for ADC
-    PRR0 &= ~(1 << PRADC);
-
-    // Select Vref = AVcc
-    ADMUX |= (1 << REFS0);
-    ADMUX &= ~(1 << REFS1);
-
-    // Set the ADC clock div by 128
-    // 16M/128=125kHz
-    ADCSRA |= (1 << ADPS0);
-    ADCSRA |= (1 << ADPS1);
-    ADCSRA |= (1 << ADPS2);
-
-    // Select Channel ADC0 (pin C0)
-    ADMUX &= ~(1 << MUX0);
-    ADMUX &= ~(1 << MUX1);
-    ADMUX &= ~(1 << MUX2);
-    ADMUX &= ~(1 << MUX3);
-
-    ADCSRA |= (1 << ADATE); // Autotriggering of ADC
-
-    // Free running mode ADTS[2:0] = 000
-    ADCSRB &= ~(1 << ADTS0);
-    ADCSRB &= ~(1 << ADTS1);
-    ADCSRB &= ~(1 << ADTS2);
-
-    // Disable digital input buffer on ADC pin
-    DIDR0 |= (1 << ADC0D);
-
-    // Enable ADC
-    ADCSRA |= (1 << ADEN);
-
-    // Start conversion
-    ADCSRA |= (1 << ADSC);
-}
-
-int main(void) {
+int main(int argc, char** argv) {
+   
     Initialize();
-    PORTB &= ~(1 << PB0);
-    PORTB &= ~(1 << PB1);
-    while (1) {
-        uint16_t adcVal = ADC;
-        printf("ADCVAL = %d\n\n", adcVal);
-        if (adcVal > 800) {
-            PORTB &= ~(1 << PB0);
-            PORTB |= (1 << PB1);
-        } else if (adcVal < 300) {
-            PORTB |= (1 << PB0);
-            PORTB &= ~(1 << PB1);
-        } else {
-            PORTB &= ~(1 << PB0);
-            PORTB &= ~(1 << PB1);
-        }
-    }
-
-#endif
     
+    uart_init();
+    adc_init();
+
+    while (1) {
+        uint16_t adcVal = adc_read();
+        printf("ADCVAL = %d\r\n", adcVal);
+
+        if (adcVal > 300) {
+            set_dir_cw();
+            send_pulse();
+        } else if (adcVal < 100) {
+            set_dir_ccw();
+            send_pulse();
+        }
+        PORTB &= ~(1 << PB0);
+        PORTB &= ~(1 << PB2);
+        PORTD &= ~(1 << PD0);
+    }
+}
+#endif
 
 #ifdef END_EFFECTOR_INTERRUPT
 /* THIS IS THE CODE FOR THE END EFFECTOR MOTOR IN INTERRUPT MODE */
@@ -464,101 +372,5 @@ ISR(PCINT0_vect) {
 int main(void) {
     Initialize();
     while (1);
-}
-#endif
-
-
-
-#ifdef GYRO
-#include <avr/io.h>
-#include <stdio.h>
-#include <stdlib.h>
-#define F_CPU 16000000UL
-#define SCL_CLOCK 100000L
-
-void TWI_init(void) {
-    // Set SCL frequency
-    TWSR0 = 0x00;
-    TWBR0 = ((F_CPU/SCL_CLOCK)-16)/2;
-
-    // Enable TWI
-    TWCR0 = (1<<TWEN);
-}
-
-void TWI_start(void) {
-    TWCR0 = (1<<TWSTA)|(1<<TWEN)|(1<<TWINT);
-    while (!(TWCR0 & (1<<TWINT)));
-}
-
-void TWI_stop(void) {
-    TWCR0 = (1<<TWSTO)|(1<<TWEN)|(1<<TWINT);
-}
-
-void TWI_write(uint8_t data) {
-    TWDR0 = data;
-    TWCR0 = (1<<TWEN)|(1<<TWINT);
-    while (!(TWCR0 & (1<<TWINT)));
-}
-
-
-uint8_t TWI_read_ack(void) {
-    TWCR0 = (1<<TWEN)|(1<<TWINT)|(1<<TWEA);
-    while (!(TWCR0 & (1<<TWINT)));
-    return TWDR0;
-}
-
-uint8_t TWI_read_nack(void) {
-    TWCR0 = (1<<TWEN)|(1<<TWINT);
-    while (!(TWCR0 & (1<<TWINT)));
-    return TWDR0;
-}
-
-
-uint8_t gyro_read_register(uint8_t reg) {
-    uint8_t data;
-    TWI_start();
-    TWI_write(0x68 << 1);
-    TWI_write(reg);
-    TWI_start();
-    TWI_write((0x68 << 1) | 1);
-    data = TWI_read_nack();
-    TWI_stop();
-    return data;
-}
-
-void main() {
-    gyro_read_register(0);
-}
-
-#include <xc.h>
-#include <util/delay.h>
-
-void Initialize() {
-    DDRB |= (1 << PB0); // pin IN1 of HW-095 motor driver
-    DDRB |= (1 << PB1); // pin IN2 of HW-095 motor driver
-    
-    // input pin (mimics commands in overall code)
-    // if PB2 is high, motor spins in one direction, if PB2 is low, motor spins in opposite direction
-    DDRB &= (1 << PB2);
-    
-    // PB3 is the kill switch. Motor will spin IF AND ONLY IF PB3 is high
-    DDRB &= (1 << PB3); 
-}
-
-int main(void) {
-    while (1) {
-        if (PINB & (1 << PINB3)) {
-            if (PINB & (1 << PINB2)) {
-                PORTB |= (1 << PB0);
-                PORTB &= ~(1 << PB1);
-            } else {
-                PORTB &= ~(1 << PB0);
-                PORTB |= (1 << PB1);
-            }
-        } else {
-            PORTB &= ~(1 << PB0);
-            PORTB &= ~(1 << PB1);
-        }
-    }
 }
 #endif
